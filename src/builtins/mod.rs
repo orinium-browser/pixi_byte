cf619c52977c82347cbbad86016c665c7882d174
ねこがくれ <156896971+nekogakure@users.noreply.github.com>
2025-12-23 15:40:02 +0900

    Builtins構造体に初期化メソッドを追加し、グローバルオブジェクトにObjectとArrayの組み込みを登録できるように更新

--- src/builtins/mod.rs
+++ src/builtins/mod.rs
@@ -1,13 +1,25 @@
 // Built-in Objects & Functions - ECMAScript標準の組み込み機能
-// TODO: Phase 2 で実装
+// Phase 2 でいくつかの最小実装を追加
 
-pub struct Builtins {
-    // TODO: 実装
-}
+pub mod object;
+pub mod array;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub struct Builtins {}
 
 impl Builtins {
     pub fn new() -> Self {
         Self {}
+    }
+
+    /// グローバルオブジェクトに組み込みを初期配置する
+    pub fn init(&self, global: &Rc<RefCell<crate::value::jsobject::JSObject>>) {
+        // Object 関連の組み込みを登録
+        self::object::install(global);
+        // Array 関連の組み込みを登録
+        self::array::install(global);
     }
 }
 
